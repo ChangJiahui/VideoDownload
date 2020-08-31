03c597836681dfdbc80d4224ea71dca22c547b9c
ChangJiahui <changjh17@mails.tsinghua.edu.cn>
2020-08-31 11:34:52 +0800

    Update BiliBiliSeriesDownload.py

--- BiliBiliSeriesDownload.py
+++ BiliBiliSeriesDownload.py
@@ -2,8 +2,8 @@
 import os
 import time
 
-base_url = "https://www.bilibili.com/video/BV1dE41177Dj?p="
-for ii in range(444, 555):
+base_url = "https://www.bilibili.com/video/BV1m7411R7CE?p="
+for ii in range(65):
     video_url = base_url + str(ii+1)
     os.system("you-get " + video_url)
     time.sleep(60)
